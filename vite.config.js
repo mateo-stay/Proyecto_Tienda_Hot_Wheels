6b3096b1ddf2eb985a43f509eff0be542be135df
mateo-stay <mate.estay@duocuc.cl>
2025-11-05 13:08:53 -0300

    error corregido

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Proyecto_Tienda_Hot_Wheels/",
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
 })
